c6bdc61f0e97107f8eb8b3a19f0efe96db303a7a
Soheb Mahmood <soheb@humboldt.co.uk>
2017-07-06 14:33:36 +0100

    relogging

--- Sources/RTMP/RTMPConnection.swift
+++ Sources/RTMP/RTMPConnection.swift
@@ -468,6 +468,8 @@ extension RTMPConnection: RTMPSocketDelegate {
             return
         }
 
+        let byteinfo = data.subdata(in: 0..<8).hexEncodedString()
+
         logger.info("Chunk info - size: \(chunk.size)" +
                 "\ntype: \(chunk.type)" +
                 "\nstreamID: \(chunk.streamId)" +
@@ -475,7 +477,7 @@ extension RTMPConnection: RTMPSocketDelegate {
                 "\nfragmented: \(chunk.fragmented)" +
                 "\nready?: \(chunk.ready)" +
                 "\ndescription: \(chunk.description)" +
-                "\nData bytes: \(String(format: "%02hhx", chunk.data[1])) \(String(format: "%02hhx", chunk.data[2])) \(String(format: "%02hhx", chunk.data[3]))" +
+                "\nData bytes: \(byteinfo)" +
                 "\nData count: \(chunk.data.count)")
 
         var position:Int = chunk.data.count
