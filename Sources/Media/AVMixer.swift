097b98db9bc115ae7408985569d7a44b68b7770e
Soheb Mahmood <soheb@humboldt.co.uk>
2018-05-09 10:54:50 +0100

    Test

--- Sources/Media/AVMixer.swift
+++ Sources/Media/AVMixer.swift
@@ -10,7 +10,7 @@ final public class AVMixer: NSObject {
         "continuousExposure",
     ]
 
-    static let defaultFPS:Float64 = 15
+    static let defaultFPS:Float64 = 60
     static let defaultVideoSettings:[NSString: AnyObject] = [
         kCVPixelBufferPixelFormatTypeKey: NSNumber(value: kCVPixelFormatType_32BGRA)
     ]
